db5f7433272aa413ef98f8c0d88d607678c5353c
SoftFever <softfeverever@gmail.com>
2022-10-12 23:37:30 +0800

    update octopus uart

--- update_octopus_uart.sh
+++ update_octopus_uart.sh
@@ -7,5 +7,5 @@ make -j
 sudo service klipper stop
 # This only copies the firmware to sd card.
 # need to power off then power on the machine(MCU) to trigger the actual flash.(SDIO limitation)
-./scripts/flash-sdcard.sh /dev/ttyAMA0 btt-octopus-pro
+./scripts/flash-sdcard.sh /dev/ttyAMA0 btt-octopus-pro-f446-v1.0
 sudo service klipper start
